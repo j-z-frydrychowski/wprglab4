51f067569b2afa35b347aa3ab836f8a6476a39f7
jzfrydrychowski <s32713@pjwstk.edu.pl>
2025-11-30 22:52:35 +0100

    Dodanie kodu filterBySeniority oraz findJubilees - 7/7 z zakładanych testów

--- src/main/java/com/test/service/EmployeeAnalytics.java
+++ src/main/java/com/test/service/EmployeeAnalytics.java
@@ -5,6 +5,7 @@ import com.test.model.Position;
 import com.test.model.Employee;
 import com.test.model.Position;
 
+import java.time.LocalDate;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -39,5 +40,25 @@ public class EmployeeAnalytics {
     public Optional<Employee> GetHighestPaidEmployee() {
         return employees.stream()
                 .max(Comparator.comparing(Employee::getSalary));
+    }
+
+    public List<Employee> filterBySeniority(int minYears, int maxYears) {
+        LocalDate now = LocalDate.now();
+        return employees.stream()
+                .filter(e -> {
+                    int seniority = e.getSeniorityInYears(now);
+                    return seniority >= minYears && seniority <= maxYears;
+                })
+                .collect(Collectors.toList());
+    }
+
+    public List<Employee> findJubilees(int intervalYears) {
+        LocalDate now = LocalDate.now();
+        return employees.stream()
+                .filter(e -> {
+                    int seniority = e.getSeniorityInYears(now);
+                    return seniority > 0 && seniority % intervalYears == 0;
+                })
+                .collect(Collectors.toList());
     }
 }
